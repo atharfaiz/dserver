87d514ab084c7231d880643588680620a99edd6d
atharfaiz <mdfaizathar@gmail.com>
2018-02-26 21:08:10 +0530

    second commit

--- src/org/test/Test.java
+++ src/org/test/Test.java
@@ -1,5 +1,20 @@
 package org.test;
 
-public class Test {
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class Test extends HttpServlet{
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+		System.out.println("this is my second commit ");
+		super.doPost(req, resp);
+	}
 
 }
